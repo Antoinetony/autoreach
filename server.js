da5fe9a768b6b82978aac65106c383473a874975
Tony <antoinetony.m11@gmail.com>
2026-03-26 19:12:22 +0300

    fix server and helmet csp

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: false
+}));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
